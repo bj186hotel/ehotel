eb02820c3bddef65b094ec950ffe83620de07922
zjc1605940753 <zjc1605940753@qq.com>
2019-06-13 10:46:56 +0800

    基础脚手架

--- src/main/java/com/bj186/ssm/controller/UserController.java
+++ src/main/java/com/bj186/ssm/controller/UserController.java
@@ -2,18 +2,12 @@ package com.bj186.ssm.controller;
 
 import com.bj186.ssm.pojo.User;
 import com.bj186.ssm.service.impl.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.Map;
 
 /**
  * @author 郑金城
@@ -31,20 +25,7 @@ public class UserController {
     public String register(@RequestBody(required = false) User user){
         System.out.println("user"+user);
         userService.register(user);
-        return "世上大师多";
+        return "";
     }
 
-
-    public String register2(@RequestBody(required = false) User user){
-        System.out.println("user"+user);
-        userService.register(user);
-        return "世上大师多";
-    }
-
-
-    public String register3(@RequestBody(required = false) User user){
-        System.out.println("user"+user);
-        userService.register(user);
-        return "世上大师多";
-    }
 }
